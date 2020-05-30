180a749d2022b87811b8fc09819fcff2600b0b82
Fabian Richter <fabian.richter@student.kit.edu>
2020-05-30 17:16:39 +0200

    maven project setup

---
+++ virage/src/main/java/com/fr2501/virage/VirageCore.java
@@ -0,0 +1,13 @@
+package com.fr2501.virage;
+
+/**
+ * Hello world!
+ *
+ */
+public class VirageCore 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ virage/src/test/java/com/fr2501/virage/TestSetup.java
@@ -0,0 +1,16 @@
+package com.fr2501.virage;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Is jUnit set up correctly?
+ */
+public class TestSetup 
+{
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
