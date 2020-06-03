89badcfe85f0e403e4d0270fb223a2dc8eed8008
Fabian Richter <fabian.richter@student.kit.edu>
2020-06-03 20:14:46 +0200

    created CompositionrRule

--- virage/src/main/java/com/fr2501/virage/types/CompositionRule.java
+++ virage/src/main/java/com/fr2501/virage/types/CompositionRule.java
@@ -1,5 +1,27 @@
 package com.fr2501.virage.types;
 
-public class CompositionRule {
+import java.util.Collection;
 
+public class CompositionRule {
+	private String name;
+	private String origin;
+	private Property succedent;
+	private Collection<Property> antecedents;
+	
+	public CompositionRule(String name, String origin, Property succedent, Collection<Property> antecedents) {
+		this.name = name;
+		this.origin = origin;
+		this.succedent = succedent;
+		this.antecedents = antecedents;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	public String getOrigin() {
+		return this.origin;
+	}
+	public Property getSuccedent() {
+		return this.succedent;
+	}
 }
