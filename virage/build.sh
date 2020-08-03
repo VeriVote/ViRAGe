4f58f47b429edb02583305016d55fccd9624c66d
Fabian Richter <fabian.richter@student.kit.edu>
2020-08-03 14:33:37 +0200

    added two bash scripts for easier setup

---
+++ virage/build.sh
@@ -0,0 +1,6 @@
+export SWI_HOME_DIR=/lib/swipl
+
+export CLASSPATH=$SWI_HOME_DIR/lib/jpl.jar:$CLASSPATH
+
+mvn package
+
---
+++ virage/target/run.sh
@@ -0,0 +1,6 @@
+export SWI_HOME_DIR=/lib/swipl
+
+export CLASSPATH=$SWI_HOME_DIR/lib/jpl.jar:$CLASSPATH
+
+java -jar ViRAGe-0.0.1-SNAPSHOT.jar
+
